3c80213332535f19bee7bf2a41b16d3fd27ab375
Rigel <pollux0718@gmail.com>
2024-08-14 01:34:31 +0900

    uvicorn execute main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def home():
     return {'message' : 'start'}
+
+
+if __name__ == "__main__" :
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
