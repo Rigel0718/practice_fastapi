930b3b2730ae263d864fabb6b9e0ec245fb7cb0f
Rigel <pollux0718@gmail.com>
2024-08-15 15:43:34 +0900

    add default value for status in users

--- app/database/orm_models.py
+++ app/database/orm_models.py
@@ -13,4 +13,4 @@ class User(Base):
     id : Mapped[int] = Column(Integer, primary_key=True, index=True)
     name : Mapped[str] = Column(String, nullable=True)
     email : Mapped[str] = Column(String, nullable=True)
-    status : Mapped[str] = Column(Enum("active", "blocked", "deleted"))
+    status : Mapped[str] = Column(Enum("active", "blocked", "deleted"), default="active")
