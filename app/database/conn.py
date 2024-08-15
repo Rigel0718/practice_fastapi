b1d327c15e64f03347a2956bb3649365593ade63
Rigel <pollux0718@gmail.com>
2024-08-15 15:19:35 +0900

    add lib to create ORM

--- app/database/conn.py
+++ app/database/conn.py
@@ -1,7 +1,7 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, Text
 from db_config import DB_URL 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 engine = create_engine(DB_URL, echo=True, pool_recycle=900)
 Session_local = sessionmaker(autoflush=True, bind=engine)
