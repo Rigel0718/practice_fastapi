ea247fdb93f9dac7768bf5d2ab072bee5f49d1c6
Rigel <pollux0718@gmail.com>
2024-08-15 21:30:21 +0900

    setting varchar in users column

--- app/database/orm_models.py
+++ app/database/orm_models.py
@@ -10,9 +10,9 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = "users"
     id : Mapped[int] = Column(Integer, primary_key=True, index=True)
-    name : Mapped[str] = Column(String, nullable=True)
-    email : Mapped[str] = Column(String, nullable=True)
-    pw : Mapped[str] = Column(String, nullable=True)
+    name : Mapped[str] = Column(String(255), nullable=True)
+    email : Mapped[str] = Column(String(255), nullable=True)
+    pw : Mapped[str] = Column(String(1000), nullable=True)
     status : Mapped[str] = Column(Enum("active", "blocked", "deleted"), default="active")
 
 if __name__  == "__main__":
