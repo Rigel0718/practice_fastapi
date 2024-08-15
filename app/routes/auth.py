c3fb3b056680e16feed8bfab238fceb10e9c80a4
Rigel <pollux0718@gmail.com>
2024-08-16 01:48:54 +0900

    make is_email_exist_session func

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.database.orm_models import get_db
-from typing import bool
+from app.database.orm_models import get_db, User
+from typing import bool, Optional
 from pydantic import EmailStr, BaseModel
 import bcrypt
 import jwt
@@ -12,6 +12,13 @@ router = APIRouter(prefix="/auth")
 class RegisterUserInform(BaseModel):
     email: EmailStr = None
     pw : str = None
+
+async def is_email_exist_session(email: str)-> bool:
+    obtained_email: Optional[str] = User.get(email=email)
+
+    if obtained_email is None:
+        return False
+    return True
 
 @router.post("/register", status_code=201)
 async def register(reg_user_info: RegisterUserInform, session: Session = Depends(get_db)):
