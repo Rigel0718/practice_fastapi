574f369b32f3f8f96a3d94300a0ce0173860799e
Rigel <pollux0718@gmail.com>
2024-08-15 14:59:42 +0900

    replace declarative_base to line 9

--- app/database/conn.py
+++ app/database/conn.py
@@ -3,7 +3,7 @@ from db_config import DB_URL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-Base = declarative_base()
-
 engine = create_engine(DB_URL, echo=True, pool_recycle=900)
 Session_local = sessionmaker(autoflush=True, bind=engine)
+
+Base = declarative_base()
