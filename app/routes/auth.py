51cbbec7fbe378e606845542b077f0a0364a0893
Rigel <pollux0718@gmail.com>
2024-08-16 02:12:07 +0900

    Implement user registeration validation logic

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from app.database.orm_models import get_db, User
 from typing import bool, Optional
 from pydantic import EmailStr, BaseModel
+from starlette.responses import JSONResponse
 import bcrypt
 import jwt
 
@@ -22,5 +23,9 @@ async def is_email_exist_session(email: str)-> bool:
 
 @router.post("/register", status_code=201)
 async def register(reg_user_info: RegisterUserInform, session: Session = Depends(get_db)):
-    is_exist: bool 
-    
+    is_exist: bool = await is_email_exist_session(reg_user_info.email)
+    if not reg_user_info.email or not reg_user_info.pw:
+        JSONResponse(status_code=400, content=dict(msg="Email and pw NOT provided"))
+    if is_exist:
+        JSONResponse(status_code=400, content=dict(msg="Email is already exist!!"))
+        
