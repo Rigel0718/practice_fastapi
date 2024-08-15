6e571dcdde2abab2b6d0a2747719959021bd5953
Rigel <pollux0718@gmail.com>
2024-08-16 02:18:13 +0900

    Implement generated_pw_hased func

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -21,6 +21,9 @@ async def is_email_exist_session(email: str)-> bool:
         return False
     return True
 
+async def generated_pw_hashed(pw: str):
+    return bcrypt.hashpw(pw.encode("utf-8"), bcrypt.gensalt())
+
 @router.post("/register", status_code=201)
 async def register(reg_user_info: RegisterUserInform, session: Session = Depends(get_db)):
     is_exist: bool = await is_email_exist_session(reg_user_info.email)
@@ -29,3 +32,4 @@ async def register(reg_user_info: RegisterUserInform, session: Session = Depends
     if is_exist:
         JSONResponse(status_code=400, content=dict(msg="Email is already exist!!"))
         
+    hashed_pw = await generated_pw_hashed(reg_user_info.pw)
