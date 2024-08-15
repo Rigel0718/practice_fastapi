17403b56f61b7a9ca093bd8c875a2b549787920f
Rigel <pollux0718@gmail.com>
2024-08-15 20:38:25 +0900

    define replace DeclarativeBase

--- app/database/orm_models.py
+++ app/database/orm_models.py
@@ -1,12 +1,11 @@
 from sqlalchemy import create_engine, Column, Integer, Text, Enum, Boolean, ForeignKey, String
 from db_config import DB_URL 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship, Mapped
-
+from sqlalchemy.orm import sessionmaker, relationship, Mapped, DeclarativeBase
 engine = create_engine(DB_URL, echo=True, pool_recycle=900)
 Session_local = sessionmaker(autoflush=True, bind=engine)
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 class User(Base):
     __tablename__ = "users"
@@ -15,3 +14,6 @@ class User(Base):
     email : Mapped[str] = Column(String, nullable=True)
     pw : Mapped[str] = Column(String, nullable=True)
     status : Mapped[str] = Column(Enum("active", "blocked", "deleted"), default="active")
+
+if __name__  == "__main__":
+    Base.metadata.create_all(bind = engine)
