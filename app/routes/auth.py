259b44800b7f17c3df79038173b280ea22854f65
Rigel <pollux0718@gmail.com>
2024-08-16 01:29:24 +0900

    create RegisterUserInform class

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,12 +1,19 @@
 from fastapi import APIRouter, Depends
-from pydantic import EmailStr
+from sqlalchemy.orm import Session
+from app.database.orm_models import get_db
+from typing import bool
+from pydantic import EmailStr, BaseModel
 import bcrypt
 import jwt
 
 
 router = APIRouter(prefix="/auth")
 
+class RegisterUserInform(BaseModel):
+    email: EmailStr = None
+    pw : str = None
+
 @router.post("/register", status_code=201)
-async def register(reg_user_info):
-    # 등록을 원하는 사용자의 정보에 중복이 있는지?
-    # pw 해싱
+async def register(reg_user_info: RegisterUserInform, session: Session = Depends(get_db)):
+    is_exist: bool 
+    
