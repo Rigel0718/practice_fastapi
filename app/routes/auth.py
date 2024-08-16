8e8dbb879c6548b1fb10604ce70cc2f91f24cc73
Rigel <pollux0718@gmail.com>
2024-08-16 10:05:16 +0900

    Add respense_model value as Token class

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -37,7 +37,7 @@ async def is_email_exist_session(email: str)-> bool:
 async def generated_pw_hashed(pw: str):
     return bcrypt.hashpw(pw.encode("utf-8"), bcrypt.gensalt())
 
-@router.post("/register", status_code=201)
+@router.post("/register", status_code=201, response_model=Token)
 async def register(reg_user_info: RegisterUserInform, session: Session = Depends(get_db)):
     is_exist: bool = await is_email_exist_session(reg_user_info.email)
     if not reg_user_info.email or not reg_user_info.pw:
