90a69547795460150a22e95429b030a1e333f520
Rigel <pollux0718@gmail.com>
2024-08-16 15:25:16 +0900

    Refactor: Update import paths

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
-from app.routes import auth
+from routes import auth
 
 app = FastAPI()
-
+app.include_router(auth.router)
 @app.get("/")
 async def home():
     return {'message' : 'start'}
 
-app.include_router(auth.router)
+
 if __name__ == "__main__" :
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
