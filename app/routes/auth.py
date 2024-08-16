e07c97aaf6f17d942ab775a5c152434cb2eebbd9
Rigel <pollux0718@gmail.com>
2024-08-16 11:37:13 +0900

    Add check pw method

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -37,6 +37,11 @@ async def is_email_exist_session(email: str)-> bool:
 async def generated_pw_hashed(pw: str):
     return bcrypt.hashpw(pw.encode("utf-8"), bcrypt.gensalt())
 
+def check_match_pw(hashed_pw, pw: str) -> bool:
+    return bcrypt.checkpw(pw.encode("utf-8"), hashed_pw)
+
+
+
 @router.post("/register", status_code=201, response_model=Token)
 async def register(reg_user_info: RegisterUserInform, session: Session = Depends(get_db)):
     is_exist: bool = await is_email_exist_session(reg_user_info.email)
@@ -46,4 +51,6 @@ async def register(reg_user_info: RegisterUserInform, session: Session = Depends
         JSONResponse(status_code=400, content=dict(msg="Email is already exist!!"))
         
     hashed_pw = await generated_pw_hashed(reg_user_info.pw)
+    if not check_match_pw(hashed_pw, reg_user_info.pw):
+        raise Exception("DO NOT Match encryped pw and normal pw")
     new_added_user = User.build_and_add(session, email=reg_user_info.email, pw=hashed_pw) #kwargs를 Enum으로 바꿔야할듯
