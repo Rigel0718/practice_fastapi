c0a229eaab26711a9b7ae78edfc0ad6eea9c65d8
Rigel <pollux0718@gmail.com>
2024-08-23 21:49:53 +0900

    chore: move schema pydantic model to schema

---
+++ app/database/schema.py
@@ -0,0 +1,21 @@
+from pydantic import EmailStr, BaseModel, ConfigDict
+from typing import Optional
+
+class RegisterUserInform(BaseModel):
+    name : str
+    email: EmailStr = None
+    pw : str = None
+
+class User(BaseModel):
+    id: int = None
+    name: Optional[str] = None
+    email: Optional[str] = None
+    pw: Optional[str] = None
+    status: str = None
+    disabled: Optional[bool] = None
+
+    model_config = ConfigDict(from_attributes=True)
+
+class Token(BaseModel):
+    Authorization_token: str 
+    token_type : str = "Bearer"
