224ead2e4e99670201945d91b582945a886a0e42
Rigel <pollux0718@gmail.com>
2024-08-28 00:05:06 +0900

    refactor: delete auth_methods in auth.py

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -10,56 +10,20 @@ import jwt
 from datetime import datetime, timedelta, timezone
 from database.schema import RegisterUserInform, User, Token
 from operators.orm_operators import get_by_email, build_and_add, get_db
+from operators.auth_operators import (
+    is_email_exist_session,
+    generated_hashed_pw,
+    check_match_pw,
+    orm2schema,
+    schema2dict,
+    build_ORM_by_schema,
+    commit_orm2db,
+    create_auth_token
+)
 
-load_dotenv(verbose=True)
-JWT_SECRET = os.getenv("JWT_SECRET")
-JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
 
 router = APIRouter(prefix="/auth", tags=['auth'])
-pw_context = CryptContext(schemes=["bcrypt"])
 
-async def is_email_exist_session(session: Session, email: str)-> bool:
-    obtained_email: Optional[str] = get_by_email(UserORM, session=session, email=email)
-    if obtained_email is None:
-        return False
-    return True
-
-def generated_hashed_pw(plain_pw: str) -> str:
-    return pw_context.hash(plain_pw)
-
-def check_match_pw(hashed_pw: str, plain_pw: str) -> bool:
-    return pw_context.verify(plain_pw, hashed_pw)
-
-def orm2schema(new_user: UserORM) -> User:
-    return User.model_validate(new_user)
-
-def schema2dict(schema: User, exclude=None):
-    return schema.model_dump(exclude=exclude)
-
-def build_ORM_by_schema(orm_model: UserORM, **kwargs):
-    orm_model_instance = orm_model()
-    for column in orm_model_instance.all_columns():
-        column_name = column.name
-        if column_name in kwargs:
-            setattr(orm_model_instance, column_name, kwargs.get(column_name))
-    return orm_model_instance
-
-
-def commit_orm2db(orm_model: UserORM, session: Session):
-    session.add(orm_model)
-    session.commit()
-    session.refresh(orm_model)
-    return orm_model
-
-
-def create_auth_token(user_data: User, expiered_delta: timedelta=timedelta(minutes=15)) -> str:
-    # _user_data_dict = user_data.model_dump(exclude={'pw'})
-    _user_data_dict = schema2dict(user_data, exclude={'pw'})
-    expire_time = datetime.now(timezone.utc) + expiered_delta
-    _user_data_dict.update({"exp" : expire_time})
-    _encode = _user_data_dict.copy()
-    jwt_encode = jwt.encode(_encode, JWT_SECRET, algorithm=JWT_ALGORITHM)
-    return jwt_encode
 
 @router.post("/register", status_code=201, response_model=Token)
 async def register(reg_user_info: RegisterUserInform, session: Annotated[Session, Depends(get_db)]):
