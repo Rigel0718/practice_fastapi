f56cd44a4678a095714a8854626c0eb143c8e016
Rigel <pollux0718@gmail.com>
2024-08-30 02:06:01 +0900

    feat: Implement JWTAuthBackend middleware

--- app/middleware/auth_access.py
+++ app/middleware/auth_access.py
@@ -1,10 +1,13 @@
+from fastapi.requests import HTTPConnection
 from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi import FastAPI, Request
 import jwt
 from dotenv import load_dotenv
 import os
-from typing import Optional
+from typing import Optional, Tuple
 from database.schema import User
+
+from starlette.authentication import BaseUser, UnauthenticatedUser, SimpleUser, AuthenticationError, AuthCredentials, AuthenticationBackend
 
 load_dotenv(verbose=True)
 JWT_SECRET = os.getenv("JWT_SECRET")
@@ -19,5 +22,20 @@ class JWTACCESSMIDDLEware(BaseHTTPMiddleware):
             request.state.user = User(**payload)
         response = await call_next(request)
         return response
+    
 
+class JWTAuthBackend(AuthenticationBackend): 
+    async def authenticate(self, conn): 
+        guest = AuthCredentials(["unauthenticated"]), UnauthenticatedUser()
+
+        if "Authorization_token" not in conn.headers:
+            return guest
+        
+        auth = conn.headers["Authorization_token"]
+        try:
+            scheme, credentials = auth.split()
+            if scheme != "Bearer":
+                return guest
+        except ValueError:
+            AuthenticationError("Invalid bearer auth credentials")
         
