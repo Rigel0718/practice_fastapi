4e16e3532f036e1a90b0bf23c2e2f3b0cb2cc94d
Rigel <pollux0718@gmail.com>
2024-09-05 03:00:09 +0900

    feat: add user route in main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 import uvicorn
 from fastapi import FastAPI, Depends
 from fastapi.security import APIKeyHeader
-from routes import auth
+from routes import auth, user
 from starlette.middleware.authentication import AuthenticationMiddleware
 from middleware.auth_access import JWTAuthBackend
 import os, sys
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
-API_KEY_HEADER = APIKeyHeader(name="Authorization", auto_error=False)
 app = FastAPI()
-app.include_router(auth.router, dependencies=[Depends(APIKeyHeader)])
+app.include_router(auth.router)
+app.include_router(user.router)
 
 app.add_middleware(AuthenticationMiddleware, backend=JWTAuthBackend())
 
